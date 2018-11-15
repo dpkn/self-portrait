622d2aefdab67564762eff3e020d6045c9ef9551
daniel <9087597+daankrs@users.noreply.github.com>
2018-11-15 19:34:22 +0100

    Added more comments

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -2,7 +2,7 @@
   <div id="phone">
     <header>
       <img src="../assets/profile.png" alt="Profile Picture">
-      <span>Dani&euml;l 2 ({{this.inactivityTimer}})</span>
+      <span>Dani&euml;l 2</span>
     </header>
     <section id="scroll">
       <div id="messages-window" v-if="messages.length>0">
@@ -35,61 +35,58 @@
     <footer>
           <input focus id="input" :placeholder="placeholder"  :disabled="this.state==2||this.state==1" type="text" v-model="newMessageContent" v-on:keyup.enter="addMessage"/>
     </footer>
+
+    <!-- The sound effects triggered by js -->
     <audio hidden id="message_receive" volume="0.1">
       <source src="message_receive.mp3" type="audio/mpeg">
     </audio>
     <audio hidden id="message_send" volume="0.1">
       <source src="message_send.mp3" type="audio/mpeg">
     </audio>
+
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 
-const TYPING = 2
-const RECEIVED = 1
+// Define the different states of the bot
+// as constanst for readability
 const NOT_TYPING = 0
+const RECEIVED = 1
+const TYPING = 2
 
 export default {
-  name: 'Home',
+  name: 'ChatWindow',
   mounted(){
    document.getElementById("input").focus();
 
-   // Since this work is shown at an exhibition,
-   // reset the state of the app when people aren't interacting with it for a minute
+   // Use the speechSynthesis API to speak the messages out loud
+   this.speachSynth = window.speechSynthesis;
+
+   // Getting the list of available voices is an async process
+   if (speechSynthesis.onvoiceschanged !== undefined) {
+     speechSynthesis.onvoiceschanged = () =>{
+       this.synthVoices = this.speachSynth.getVoices();
+     }
+   }
+
+   // Since this work is shown at an exhibition, it includes a function to
+   // reset the state of the app when people aren't interacting with it for some time
+   // Every second it checks if the limit is reached
    window.setInterval(()=>{
      this.inactivityTimer++;
      if(this.inactivityTimer>=this.inactivtyLimit){
-       this.inactivityTimer = 0;
-       document.querySelector('#message_receive').play()
-       this.messages.push({user_id: 2, content: 'Hallo waarom praat niemand tegen mij.'});
-
-       setTimeout(() => {
-         this.speak('Hallo waarom praat niemand tegen mij.',0);
-       }, 500);
-       //this.reset()
+       this.reset();
+       // Pick a random idle message and speak it out loud to grab attention
+       this.speak(this.idleMessages[Math.floor(Math.random()*this.idleMessages.length)],0);
      }
    }, 1000);
-   document.onkeypress = ()=>{
+
+   // When the app is being interacted with, reset the timer
+   document.onkeypress = () =>{
      this.inactivityTimer = 0;
    }
-
-  this.speachSynth = window.speechSynthesis;
-  this.synthVoices = this.speachSynth.getVoices();
-
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = () =>{
-      this.synthVoices = this.speachSynth.getVoices();
-      console.log(this.synthVoices)
-    }
-  }
-
-  // Create the LSTM Generator passing it the model directory
-  this.lstm = ml5.LSTMGenerator('models/input/', (e)=>{
-    console.log('model readye',e)
-  });
-
 
   },
   data() {
@@ -99,14 +96,15 @@ export default {
       state: NOT_TYPING,
       placeholder:'Type something and press ENTER to send',
       inactivityTimer:0,
-      inactivtyLimit:20000
+      inactivtyLimit:120, // Amount of s that the app will wait until it resets itself,
+      serverURI:'http://127.0.0.1:5000', // URI of the back-end that supplies messages
+      idleMessages: ['Ik voel me eenzaam.','Hallo, waarom praat niemand tegen mij','Mag ik wat aandacht alsjeblieft'],
     }
   },
   methods: {
-    // This function uses the SpeechSynthesis API to read the replies out loud
-    speak(text,voice){
+    // This function uses the SpeechSynthesis API to speak text
+    speak(text = '',voice = 0){
       if (this.speachSynth.speaking) {
-          console.error('speechSynthesis.speaking');
           return;
       }else{
         let utterThis = new SpeechSynthesisUtterance(text);
@@ -114,8 +112,7 @@ export default {
         this.speachSynth.speak(utterThis);
       }
     },
-    generate(){
-    },
+    // Resets the chat window to its original state
     reset:function(){
       this.newMessageContent = ''
       this.messages = []
@@ -123,68 +120,83 @@ export default {
       this.inactivityTimer = 0;
       this.placeholder = 'Type something and press ENTER to send'
     },
+    // Adds a message typed by the user to the chat window
     addMessage: function() {
       if(this.newMessageContent.length<1)
       return
 
       this.messages.push({user_id: 1, content: this.newMessageContent});
+
+      // Play send sound and let the text be spoken
       document.querySelector('#message_send').play()
       this.speak(this.newMessageContent,10);
 
       this.newMessageContent = '';
-      var that =this;
 
       this.state = RECEIVED
       this.placeholder = 'Waiting for response...'
 
-          axios.get('http://127.0.0.1:5000/message')
-            .then(function (response) {
+      axios.get(this.serverURI+'/message').then((response) =>{
 
-              setTimeout(function(){
-                that.state = TYPING
-                setTimeout(function(){
-                  document.querySelector('#message_receive').play()
-                  that.messages.push({user_id: 2, content: response.data.message});
+          let reply = response.data.message
 
-                  setTimeout(() => {
-                    that.speak(response.data.message,0);
-                  }, 500);
+          setTimeout(()=>{
+            this.state = TYPING
+            let typingTime = 900 + reply.length*10
 
-                  that.state = NOT_TYPING
-                  that.placeholder = 'Type something and press ENTER to send'
-                  that.inactivityTimer = 0;
-                }, 2000);
+            setTimeout(()=>{
+              document.querySelector('#message_receive').play()
+              this.messages.push({user_id: 2, content: reply});
+
+              this.state = NOT_TYPING
+              this.placeholder = 'Type something and press ENTER to send'
+              this.inactivityTimer = 0;
+
+              setTimeout(() => {
+                this.speak(reply,0);
               }, 400);
 
-            })
-            .catch(function (error) {
-              // handle error
-              console.error(error);
-            })
+            }, typingTime);
+
+          }, 600);
+
+        })
+        .catch(function (error) {
+          console.error(error);
+        })
 
       }
   },
   updated(){
     // Everytime a new message is added, scroll to the bottom so it is in view
     // And refocus the input so a mouse isn't needed
-     var scrollElement = document.getElementById("scroll");
-     scrollElement.scrollTop = scrollElement.scrollHeight;
-     document.getElementById("input").focus();
+    var scrollElement = document.getElementById("scroll");
+    scrollElement.scrollTop = scrollElement.scrollHeight;
+    document.getElementById("input").focus();
   },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.emptyState{
-  padding: 50px;
-  padding-top: 150px;
-  text-align: center;
+#phone{
+  max-width: 375px;
+  border-radius: 15px;
+  margin: 0 auto;
+  height: 652px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  position: relative;
+  overflow:hidden;
+  background: #fff;
+  display: flex;
+  flex-flow: column;
 }
+
 header,footer{
 	background: rgba(250, 250, 250, 1);
   backdrop-filter:blur(10px);
 }
+
 header{
   position: absolute;
   height: 75px;
@@ -195,6 +207,7 @@ header{
   border-radius: 15px 15px 0 0;
   font-size: 0.9em;
 }
+
 header img{
     background: #f2f2f2;
     width: 40px;
@@ -203,6 +216,7 @@ header img{
     margin: 5px auto;
     display: block;
 }
+
 footer{
   position: absolute;
   bottom: 0;
@@ -216,6 +230,7 @@ footer{
   box-sizing: border-box;
   padding: 7px;
 }
+
 input{
   border-radius: 15px;
   height: 30px;
@@ -225,27 +240,16 @@ input{
   outline: none;
   padding: 20px;
 }
-#phone{
-  max-width: 375px;
-  border-radius: 15px;
-  margin: 0 auto;
-  height: 652px;
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-  position: relative;
-  overflow:hidden;
-  background: #fff;
-  display: flex;
-  flex-flow: column;
-  margin-top: 40px;
-}
+
 ::scrollbar {
     display: none;
 }
-::-webkit-scrollbar { display: none; }
+
 #scroll{
     overflow:scroll;
     scroll-behavior: smooth;
 }
+
 #messages-window {
   flex: 1 0 auto;
   display: flex;
@@ -270,7 +274,10 @@ input{
   align-self:flex-end;
 }
 
-/* TYPING */
+/* TYPING ANIMATION
+   Inspired by Joseph Fusco
+   https://codepen.io/fusco/pen/XbpaYv
+*/
 .typing-indicator {
   padding: 12px;
   -webkit-animation: 2s bulge infinite ease-out;
@@ -286,6 +293,7 @@ input{
   border-radius: 50%;
   opacity: 0.4;
 }
+
 .typing-indicator span:nth-of-type(1) {
   -webkit-animation: 1s blink infinite 0.3333s;
           animation: 1s blink infinite 0.3333s;
@@ -310,6 +318,7 @@ input{
     opacity: 1;
   }
 }
+
 @-webkit-keyframes bulge {
   50% {
     -webkit-transform: scale(1.05);
@@ -322,6 +331,4 @@ input{
             transform: scale(1.05);
   }
 }
-
-
 </style>
